78a711c330e9c52418e5200bbb738280762b8150
berkc <b.cal.2193@gmail.com>
2022-05-23 22:39:20 +0200

    23.05

---
+++ src/test/java/task/Questıons3/Q10.java
@@ -0,0 +1,21 @@
+package task.Questıons3;
+
+public class Q10 {
+    //● Bir class oluşturun : C02_WebTables
+    //● login( ) metodun oluşturun ve oturum açın.
+    //● https://www.hotelmycamp.com admin/HotelRoomAdmin adresine gidin
+    //
+    //○ Username : manager
+    //○ Password : Manager1!
+    //
+    //● table( ) metodu oluşturun
+    //
+    //○ Tüm table body’sinin boyutunu(sutun sayisi) bulun. /tbody
+    //○ Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
+    //
+    //● printRows( ) metodu oluşturun //tr
+    //
+    //○ table body’sinde bulunan toplam satir(row) sayısını bulun.
+    //○ Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
+    //○ 4.satirdaki(row) elementleri konsolda yazdırın.
+}
