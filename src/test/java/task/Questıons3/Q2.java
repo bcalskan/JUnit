fa98711ed3e90ece4d7c583aa05f9b24d68d788c
berkc <b.cal.2193@gmail.com>
2022-05-23 18:30:08 +0200

    23,05

---
+++ src/test/java/task/Questıons3/Q2.java
@@ -0,0 +1,9 @@
+package task.Questıons3;
+
+public class Q2 {
+    //Bilgisayarımızda bir dosya olup olmadığını(exist) kontrol etmek için Java'yı kullanabiliriz
+    //○ System.getProperty ( "user.dir"); icinde bulunulan klasörün yolunu (Path) verir
+    //○ System.getProperty ( "user.home"); bilgisayarimizda bulunan user klasörünü verir
+    //○ Files.exists (Paths.get (filePath)); Bilgisayarınızda dosyanın olup olmadığını kontrol eder
+
+}
---
+++ src/test/java/task/Questıons3/Q3.java
@@ -0,0 +1,10 @@
+package task.Questıons3;
+
+public class Q3 {
+    //1. Tests packagenin altina bir class oluşturalim : C04_FileDownload
+    //2. Iki tane metod oluşturun : isExist( ) ve downloadTest( )
+    //3. downloadTest ( ) metodunun icinde aşağıdaki testi yapalim:
+    //      - https://the-internet.herokuapp.com/download adresine gidelim.
+    //      - code.txt dosyasını indirelim
+    //4. Ardından isExist( ) methodunda dosyanın başarıyla indirilip indirilmediğini test edelim
+}
